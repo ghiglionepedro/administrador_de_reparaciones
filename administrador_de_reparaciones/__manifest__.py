9067c27211db3da116fb712fb3a88a1eac4c6baf
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2022-12-20 10:33:56 -0300

    Correcciones en el revert

--- administrador_de_reparaciones/__manifest__.py
+++ administrador_de_reparaciones/__manifest__.py
@@ -29,7 +29,6 @@
         'data/data.xml',
         'views/views_marca.xml',
         'views/views_modelo.xml',
-        'views/views_partner.xml',
         'views/views_tipo_producto.xml',
     ],
     # only loaded in demonstration mode
