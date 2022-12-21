9b557a16b1463938b8ebb10ee0267fa6935330e0
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2022-12-21 01:10:49 -0300

    Mejorando reparaciones en el form de product.product

--- administrador_de_reparaciones/models/product.py
+++ administrador_de_reparaciones/models/product.py
@@ -5,8 +5,8 @@ class administrador_reparaciones_product(models.Model):
     _inherit = 'product.product'
 
     
-    tipo_equipo_id = fields.Many2one('product.product.tipo.equipo', string='Equipo', required=True)
-    marca_id = fields.Many2one('product.product.marca', string='Tipo de Producto', required=True)
+    tipo_equipo_id = fields.Many2one('product.product.tipo.equipo', string='Tipo de equipo', required=True)
+    marca_id = fields.Many2one('product.product.marca', string='Marca', required=True)
     modelo_id = fields.Many2one('product.product.modelo', string='Modelo', required=True)
 
 class administrador_reparaciones_product_template(models.Model):
