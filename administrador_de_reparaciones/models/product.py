1f225466ea5e59ec459800b7e018ac91b66d9e48
Pedro Matias Ghiglione <pedromatiasghiglione@gmail.com>
2022-12-23 00:07:59 -0300

    cambiando los many2one por many2many

--- administrador_de_reparaciones/models/product.py
+++ administrador_de_reparaciones/models/product.py
@@ -5,14 +5,14 @@ class administrador_reparaciones_product(models.Model):
     _inherit = 'product.product'
 
     
-    tipo_equipo_id = fields.One2many('product.product.tipo.equipo', string='Tipo de equipo')
-    marca_id = fields.One2many('product.product.marca', string='Marca')
-    modelo_id = fields.One2many('product.product.modelo', string='Modelo')
+    tipo_equipo_id = fields.Many2many('product.product.tipo.equipo', string='Tipo de equipo')
+    marca_id = fields.Many2many('product.product.marca', string='Marca')
+    modelo_id = fields.Many2many('product.product.modelo', string='Modelo')
 
 class administrador_reparaciones_product_template(models.Model):
     _inherit = 'product.template'
 
     
-    tipo_equipo_id = fields.One2many('product.product.tipo.equipo', string='Tipo de equipo')
-    marca_id = fields.One2many('product.product.marca', string='Marca')
-    modelo_id = fields.One2many('product.product.modelo', string='Modelo')
+    tipo_equipo_id = fields.Many2many('product.product.tipo.equipo', string='Tipo de equipo')
+    marca_id = fields.Many2many('product.product.marca', string='Marca')
+    modelo_id = fields.Many2many('product.product.modelo', string='Modelo')
